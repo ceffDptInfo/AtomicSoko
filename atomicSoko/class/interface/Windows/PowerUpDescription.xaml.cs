cbb7d85ede2c6bb32be213126bd76776412728fc
FabDarb <Fabien.Darbellay@ceff.ch>
2024-05-14 09:29:24 +0200

    modification text cash doubler

--- atomicSoko/class/interface/Windows/PowerUpDescription.xaml.cs
+++ atomicSoko/class/interface/Windows/PowerUpDescription.xaml.cs
@@ -32,7 +32,7 @@ namespace atomicSoko
         {
             Description.Add(PowerUps.CellThief, "Vole la cellule d'un adversaire sans rajouter de cellule");
             Description.Add(PowerUps.WallDestroyer, "Casse un mur");
-            Description.Add(PowerUps.CashDoubler, "Il y a 4 possibilités pour votre argent si vous l'utilisez sois vous gagnez et vous faite x2 sois vous perdez et c'est x0 si vous ne l'utilisez pas sois vous gagnez et vous faite x1,5 ou vous perdez et faite +50% de se que le gagnant a gagné");
+            Description.Add(PowerUps.CashDoubler, "Chaque fois utiliser durant la partie vous augmentera votre multiplicateur pour la fin de la partie si vous gagnez");
             Description.Add(PowerUps.NeutralNuke, "Vous faite apparaître des cellules neutres que tout le monde peut faire exploser --> les cellules sont a une cellule d'exploser");
             Description.Add(PowerUps.Angelica, "Protège votre ou la cellule cliqué pour 3 tour auquel rien ne peut la touché ni même le joueur");
             Description.Add(PowerUps.ElJutos, "Vous pouvez cliquer sur un trou noir qui fera apparaître vos cellules autour");
